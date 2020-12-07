af9b25de286528dc6903912545311654ac824b5b
Arivlorn <60198052+Arivlorn@users.noreply.github.com>
2020-12-07 16:10:20 -0600

    Added functionality to read maze edges from external .txt file. Added all edges of given maze to file and created working solutions for steps 1 through 4 of Directions through Maze portion of assignment.

--- src/GraphExample.java
+++ src/GraphExample.java
@@ -1,16 +1,67 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-/** Test a simple implementation of an adjacency list representation 
-  * of a graph.
-  * @author rlsummerscales
-  * */
+/**
+ * Test a simple implementation of an adjacency list representation
+ * of a graph.
+ * @author rlsummerscales, Anthony Isensee
+ * */
 public class GraphExample {
-    public static void main(String[] args){
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // create graph
+        UndirectedGraph u = new UndirectedGraph();
+
+        // add edges from file
+        try {
+
+            // create needed objects
+            File mazeEdges = new File("res\\mazeEdges.txt");
+            Scanner myScanner = new Scanner(mazeEdges);
+
+            // read file to edges
+            while (myScanner.hasNextLine()) {
+                String vertex1 = "(" + myScanner.nextLine() + ")";
+                String vertex2 = "(" + myScanner.nextLine() + ")";
+
+                // skip whitespace separating edges
+                if (myScanner.hasNextLine()) {
+                    myScanner.nextLine();
+                }
+
+                // add file information to graph
+                u.addEdge(vertex1, vertex2);
+            }
+
+            // close file
+            myScanner.close();
+
+        } catch(FileNotFoundException e) {
+            System.out.println("File read error. Check format.");
+        }
+
+
+        //u.display();
+        u.breadthFirstSearch("(0,0)");
+        u.printPath("(0,0)", "(7,7)");
+
+        // clear information
+        u.clear();
+
+
+
+
+        /*
+
         UndirectedGraph g = new UndirectedGraph();
         g.addEdge("alice", "bob");
         g.addEdge("alice", "carol");
         g.addEdge("bob", "carol");
         g.addEdge("alice", "eve");
         g.display();
+        g.depthFirstSearch("eve");
+        g.printPath("eve", "carol");
         g.clear();
         
         g.addEdge("s", "a");
@@ -44,5 +95,9 @@ public class GraphExample {
         g.printPath("s", "d");
         System.out.println();    
         g.clear();
+
+
+
+         */
     } 
 }
